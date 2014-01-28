f07ad6af8eba3853d64d66f0053d8e160a6dca11
Robert Malko <robmalko@gmail.com>
2014-01-28 15:20:23 -0500

    example of wrapper classes approach

--- library/classes.js
+++ library/classes.js
@@ -1,4 +1,25 @@
 var Document = {}
 
-Document.artboards = [doc artboards]
-Document.pages = [doc pages]
+Document.artboards = function(){
+  return _.map([doc artboards], function(artboard){
+    return new Artboard(artboard)
+  })
+}
+
+Document.pages = function(){
+  return _.map([doc pages], function(page){
+    return new Page(page)
+  })
+}
+
+var Artboard = function(artboard){
+  this.orig = artboard
+
+  this.name = [artboard name]
+}
+
+var Page = function(page){
+  this.orig = page
+
+  this.name = [page name]
+}
